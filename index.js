d6e3b36f4db82e0240b71363c8e56aa58230a2f0
Darknessmentorify <guillermoherrera435@gmail.com>
2026-04-13 11:19:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+// Cuando alguien escribe cualquier cosa
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Bot activo 🚀');
+    bot.sendMessage(msg.chat.id, "Bot activo 🚀");
+});
+
+// Comando /start
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Bienvenido 🔥");
 });
